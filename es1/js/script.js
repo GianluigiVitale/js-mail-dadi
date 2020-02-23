f0605d75d849408d87f726ac3fa1609514b81c51
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-23 18:04:05 +0100

    added rules

--- es1/js/script.js
+++ es1/js/script.js
@@ -1,3 +1,11 @@
+/*
+Scopo del gioco:
+Chiedi all’utente la sua email controlla che sia
+nella lista di chi può accedere e stampa un
+messaggio appropriato.
+
+*/
+
 var email = prompt('Scrivi la tua email')
 
 var listaEmail = ['mario@rossi.it', 'francesco@rossi.it', 'saverio@rossi.it'];
